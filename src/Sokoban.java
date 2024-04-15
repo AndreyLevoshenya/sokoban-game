406c6f07d0ab59500a3ff9c8d47794efaaa502fb
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2024-04-15 14:44:33 +0300

    added menu and fixed bugs

--- src/Sokoban.java
+++ src/Sokoban.java
@@ -1,22 +1,7 @@
 import graphics.GraphicInterface;
-import logic.Game;
-import logic.LevelBuilder;
-import utils.Utils;
-
-import java.io.InputStream;
-
-import static utils.Constants.LEVELS_INFO;
-import static utils.Constants.SOMETHING_WENT_WRONG;
 
 public class Sokoban {
     public static void main(String[] args) {
-        try {
-            InputStream in = Utils.getResource(LEVELS_INFO);
-            LevelBuilder levelBuilder = new LevelBuilder(in);
-            Game game = new Game(levelBuilder);
-            GraphicInterface.start(game);
-        } catch (Exception e) {
-            System.err.println(SOMETHING_WENT_WRONG + e);
-        }
+        GraphicInterface.start();
     }
 }
